9bf63f0deb090a8fc70eaee00b65d5308b5387bf
sappy <sappy5678@gmail.com>
2017-01-20 09:24:18 +0800

    test

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='FlaskApp',
       author='Ryan Jarvinen',
       author_email='ryanj@redhat.com',
       url='http://www.python.org/sigs/distutils-sig/',
-     install_requires=['Flask>=0.10.1'],
+     install_requires=['Flask>=0.10.1','raven>=5.32.0','Flask-Admin>=1.4.2'],
      )
